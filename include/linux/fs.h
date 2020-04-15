14e832e5c5b95c06f52d09c4306f7ee990e555c9
Deven Bowers <deven.desai@linux.microsoft.com>
2020-04-15 09:25:43 -0700

    fs: add security blob and hooks for block_device
    Add a security blob and associated allocation, deallocation and set hooks
    for a block_device structure.
    
    Signed-off-by: Deven Bowers <deven.desai@linux.microsoft.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -506,6 +506,7 @@ struct block_device {
 	int			bd_fsfreeze_count;
 	/* Mutex for freeze */
 	struct mutex		bd_fsfreeze_mutex;
+	void			*security;
 } __randomize_layout;
 
 /* XArray tags, for tagging dirty and writeback pages in the pagecache. */
